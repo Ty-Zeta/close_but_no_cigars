e9f33f8fe55a7e7e7b6e723130ee9f34f3a5726c
Ty-Zeta <tyofzeta@gmail.com>
2017-07-13 17:01:36 -0400

    added tests that were grey

--- close_no_cigar_test.rb
+++ close_no_cigar_test.rb
@@ -2,16 +2,16 @@ require "minitest/autorun"
 require_relative "close_no_cigar.rb"
 
 class TestCloseNoCigar < Minitest::Test
-  # def test_nothing
-  #   my_num = "1234"
-  #   win_num = []
-  #   assert_equal([], lottery(my_num, win_num))
-  # end
-  # def test_winning_num_match
-  #   my_num = "1234"
-  #   win_num = ["1234", "4567", "8901", "2345", "6789"]
-  #   assert_equal(["1234"], lottery(my_num, win_num))
-  # end
+  def test_nothing
+    my_num = "1234"
+    win_num = []
+    assert_equal([], lottery(my_num, win_num))
+  end
+  def test_winning_num_match
+    my_num = "1234"
+    win_num = ["1234", "4567", "8901", "2345", "6789"]
+    assert_equal(["1234"], lottery(my_num, win_num))
+  end
   def test_close_match
     my_num = "1234"
     win_num = ["1244"]
