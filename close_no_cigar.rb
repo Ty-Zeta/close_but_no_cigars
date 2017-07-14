51fde411aa3c617a5938c9b0a0c2f4601547a5f7
Ty-Zeta <tyofzeta@gmail.com>
2017-07-14 14:29:20 -0400

    Now matches all 4#'s in any order

--- close_no_cigar.rb
+++ close_no_cigar.rb
@@ -16,14 +16,28 @@ def off_by_one(my_num, win_num1)
     win_num1.length.times do
       if my_num[z] == win_num1[i]
         count += 1
-        #p count
       end
-      #puts i
       i += 1
   end
   z += 1
 end
   count == win_num1.length - 1
+end
+
+def all_four_wrong_order(my_num, win_num1)
+  count = 0
+  z = 0
+   win_num1.length.times do
+     i = 0
+    win_num1.length.times do
+      if my_num[z] == win_num1[i]
+        count += 1
+      end
+      i += 1
+  end
+  z += 1
+end
+  count == win_num1.length
 end
 
 def almost_there(my_num, win_num1)
@@ -36,86 +50,12 @@ def almost_there(my_num, win_num1)
   winning
 end
 
-# if my_num[i] == win_num1[i]
-#   count += 1
-# end
-#   i += 1
-# end
-
-#   if my_num[0] == win_num1[0]
-#     count += 1
-#   else 0
-#   end
-#   if my_num[1] == win_num1[1]
-#     count +=1
-#   else 0
-#   end
-#   if my_num[2] == win_num1[2]
-#     count +=1
-#   else 0
-#   end
-#   if my_num[3] == win_num1[3]
-#     count +=1
-#   else 0
-#   end
-#   if count == 3
-#     true
-#   end
-# end
-
-# def Winning (my_num, win_num)
-#   matches = []
-#
-
-# my_num = "1234"
-# win = ["1234", "4567", "8901", "2345", "6789"]
-# def off_by_one(win, my_num)
-#   x = 0
-#   count = 0
-#   win.length.times do
-#     if my_num[x] == win[x]
-#       count += 1
-#     end
-#     x += 1
-#   end
-#   count == win.length - 1
-# end
-#
-# def lottery(my_num, win_num)
-#   match = []
-#   win_num.each do |win|
-#     if off_by_one(win, my_num)
-#       match << win
-#     end
-#   end
-#   p match
-# end
-# p lottery(my_num, win)
-
-
-# def one_by_one
-#   match = []
-#   win_num.to_s.map { |x|
-#     x.to_s.split
-#   }
-#     if my_num == x
-#       match << my_num
-#     end
-#   end
-#   p match
-
-
-# def off_by_one
-#   match = []
-#   counter = 0
-#   win_num.each_scanc(4)
-#   if counter = 4
-#     match <<
-
-# def addstwonumbers(numberone, numbertwo)
-#   sum = numberone + numbertwo
-#   sum
-#   puts sum
-# end
-#
-# addstwonumbers(1,5)
+def matching_but_wrong_order(my_num, win_num1)
+  winner = []
+  win_num1.each do |x|
+    if all_four_wrong_order(my_num, x) == true
+      winner << x
+    end
+  end
+    winner
+end
--- close_no_cigar_test.rb
+++ close_no_cigar_test.rb
@@ -14,12 +14,17 @@ class TestCloseNoCigar < Minitest::Test
   end
   def test_close_match
     my_num = "1234"
-    win_num = ["1244"]
+    win_num = ["3412"]
     assert_equal(true, off_by_one(my_num, win_num))
   end
   def test_close_no_cigar
     my_num = "1234"
-    win_num = ["1235", "4567", "8901", "2345", "6789", "9234", "9123", "3412"]
-    assert_equal(["1235", "2345","9234","9123"],almost_there(my_num, win_num))
+    win_num = ["1235", "4567", "2345", "6789", "9234", "9312", "3412"]
+    assert_equal(["1235", "2345","9234","9312",],almost_there(my_num, win_num))
+  end
+  def test_all_4_num_not_order
+    my_num = "1234"
+    win_num = ["3412", "5678", "2345", "2143"]
+    assert_equal(["3412", "2143"],matching_but_wrong_order(my_num, win_num))
   end
  end
